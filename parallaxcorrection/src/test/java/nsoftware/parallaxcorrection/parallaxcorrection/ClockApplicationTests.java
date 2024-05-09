6b5a66c526913ef74e1776a2801d7704febebba0
asg5450 <asg5450@gmail.com>
2024-05-09 09:56:31 +0900

    Spring Project Creation
    스프링 프로젝트 생성
    - thymeleaf
    - JDBC
    - Web
    - Lombok
    - MySQL Driver

---
+++ parallaxcorrection/src/test/java/nsoftware/parallaxcorrection/parallaxcorrection/ClockApplicationTests.java
@@ -0,0 +1,13 @@
+package nsoftware.parallaxcorrection.parallaxcorrection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
