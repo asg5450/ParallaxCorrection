21d5da260117139385a078b1710d9e1a9c201184
asg5450 <asg5450@gmail.com>
2024-05-12 19:55:41 +0900

    timeDifferenceDB메소드 DTO 제거
    timeDifferenceDB메소드 DTO 제거

--- syncclock/src/main/java/com/timesyncronize/syncclock/controller/ClockApiController.java
+++ syncclock/src/main/java/com/timesyncronize/syncclock/controller/ClockApiController.java
@@ -1,9 +1,7 @@
 package com.timesyncronize.syncclock.controller;
 
 import com.timesyncronize.syncclock.model.request.BrowserWasTimeSyncRequest;
-import com.timesyncronize.syncclock.model.request.DatabaseTimeRequest;
 import com.timesyncronize.syncclock.model.response.BrowserWasTimeSyncResponse;
-import com.timesyncronize.syncclock.model.response.DatabaseTimeResponse;
 import com.timesyncronize.syncclock.service.ClockApiService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -21,21 +19,7 @@ public class ClockApiController {
     }
 
     @PostMapping("/dbNow")
-    public Long timeDifferenceDB(DatabaseTimeRequest<Long> request){
-        request.setWasBefore(System.currentTimeMillis());
-        System.out.println("db 요청 전: " + request.getWasBefore());
-
-        DatabaseTimeResponse<Long> dbDate = clockApiService.timeDifferenceDB(request);
-        System.out.println("db: " + dbDate);
-
-//        long wasAfter = System.currentTimeMillis();
-//        System.out.println("db 요청 후: " + wasAfter);
-
-        Long sysdateDifference = ((System.currentTimeMillis() - dbDate.getWasBefore()) / 2);
-        System.out.println("질의 요청 소요 시간: " + sysdateDifference);
-        
-        Long result = (dbDate.getDbNow() - dbDate.getWasBefore() - sysdateDifference);
-        System.out.println("was-db 시간 차이: " + result);
-        return result;
+    public Long timeDifferenceDB(){
+        return clockApiService.timeDifferenceDB();
     }
 }
--- syncclock/src/main/java/com/timesyncronize/syncclock/service/ClockApiService.java
+++ syncclock/src/main/java/com/timesyncronize/syncclock/service/ClockApiService.java
@@ -1,9 +1,7 @@
 package com.timesyncronize.syncclock.service;
 
 import com.timesyncronize.syncclock.model.request.BrowserWasTimeSyncRequest;
-import com.timesyncronize.syncclock.model.request.DatabaseTimeRequest;
 import com.timesyncronize.syncclock.model.response.BrowserWasTimeSyncResponse;
-import com.timesyncronize.syncclock.model.response.DatabaseTimeResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
@@ -16,9 +14,30 @@ public class ClockApiService {
         return new BrowserWasTimeSyncResponse<Long>(request.getBrowserBefore(), System.currentTimeMillis());
     }
 
-    public DatabaseTimeResponse<Long> timeDifferenceDB(DatabaseTimeRequest<Long> request){
-        //Long wasBefore = System.currentTimeMillis();
+    public Long timeDifferenceDB(){
+
+        // WAS Before 시간 확인
+        Long wasBefore = System.currentTimeMillis();
+
+        // DB 시간 확인
         Long dbNow = jdbcTemplate.queryForObject("select UNIX_TIMESTAMP(sysdate()) * 1000;" ,Long.class);
-        return new DatabaseTimeResponse<Long>(request.getWasBefore(), dbNow);
+
+        // WAS After 시간 확인
+        Long wasAfter = System.currentTimeMillis();
+
+        // WAS <-> DB 요청 소요 시간
+        Long sysdateRequestTime = ((wasAfter - wasBefore) / 2);
+
+        // 디버깅 출력문
+        System.out.println(" wasBefore = " + wasBefore);
+        System.out.println(" dbNow = " + dbNow);
+        System.out.println(" wasAfter = " + wasAfter);
+        System.out.println("질의 요청 소요 시간: " + sysdateRequestTime);
+
+        // WAS <-> DB 시차
+        Long result = (dbNow - wasBefore - sysdateRequestTime);
+        System.out.println("WAS <-> DB 시간 차이: " + result);
+
+        return result;
     }
 }
